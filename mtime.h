3c15fa31f2f4aa9b24a69cfc5c9496199c75b193
Eduardo Costa <m4c0@users.noreply.github.com>
2024-03-19 10:47:41 +0000

    poc + fix build on c compilers

--- mtime.h
+++ mtime.h
@@ -3,20 +3,20 @@
 
 uint64_t mtime_of(const char *filename) {
 #if _WIN32
-  struct __stat64 s {};
+  struct __stat64 s;
   _stat64(filename, &s);
   return s.st_mtime;
 #elif __APPLE__
-  struct stat s {};
+  struct stat s;
   stat(filename, &s);
-  auto mtime = s.st_mtimespec;
-  return static_cast<uint64_t>(mtime.tv_sec) * 1000000000ul +
-         static_cast<uint64_t>(mtime.tv_nsec);
-#else
-  struct stat s {};
+  uint64_t sec = s.st_mtimespec.tv_sec;
+  uint64_t nsec = s.st_mtimespec.tv_nsec;
+  return sec * 1000000000ul + nsec;
+#else // Assume POSIX
+  struct stat s;
   stat(filename, &s);
-  auto mtime = s.st_mtim;
-  return static_cast<uint64_t>(mtime.tv_sec) * 1000000000ul +
-         static_cast<uint64_t>(mtime.tv_nsec);
+  uint64_t sec = s.st_mtim.tv_sec;
+  uint64_t nsec = s.st_mtim.tv_nsec;
+  return sec * 1000000000ul + nsec;
 #endif
 }
---
+++ poc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define MTIME_IMPLEMENTATION
+#define MTIME_ERROR(x) puts
+#include "mtime.h"
+
+void test(const char * file){
+  uint64_t m = mtime_of(file);
+  printf("Modification time of [%s]: %llu\n", file, m);
+}
+
+int main() {
+  test("poc.cpp");
+  test("mtime.h");
+  return 0;
+}
