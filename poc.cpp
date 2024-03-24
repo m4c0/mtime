af535f6e8faf515b769fcd55c2e549dc32d1f882
Eduardo Costa <m4c0@users.noreply.github.com>
2024-03-24 07:31:50 +0000

    poc: fix formatting

--- poc.cpp
+++ poc.cpp
@@ -5,7 +5,7 @@ import silog;
 #define MTIME_ERROR(x) silog::log(silog::error, "Error: %s", (x))
 #include "mtime.h"
 
-void test(const char * file){
+void test(const char *file) {
   auto m = mtime_of(file);
   silog::log(silog::info, "Modification time of [%s]: %llu", file, m);
 }
